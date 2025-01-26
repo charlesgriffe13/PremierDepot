36eb2a575c5b4f756ab14d852ef86f9e08a00a35
charlesgriffe <charlesgriffe@recursive974.com>
2025-01-26 14:11:23 +0400

    commit2 : MessageBox

--- main.py
+++ main.py
@@ -15,6 +15,13 @@ class MainWindow(QMainWindow):
         btnMessage1.move(20, 150)
         btnMessage1.clicked.connect(lambda: self.evt_btnMessage_clicked(btnMessage1.text()))
 
+        # Bouton 2
+        btnMessage2 = QPushButton(self)
+        btnMessage2.setFixedSize(150, 40)
+        btnMessage2.setText('Message 2')
+        btnMessage2.move(20, 250)
+        btnMessage2.clicked.connect(lambda: self.evt_btnMessage_clicked(btnMessage2.text()))
+
     def evt_btnMessage_clicked(self, msg):
         # Création de la boîte de message
         msgBox = QMessageBox()
