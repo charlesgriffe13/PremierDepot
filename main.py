43f781441a2690c5f105345513d3f93febb01c8f
charlesgriffe <charlesgriffe@recursive974.com>
2025-01-27 10:13:42 +0400

    commit3 : coloration des boutons

--- main.py
+++ main.py
@@ -3,24 +3,48 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
+class LblColor(QPushButton):
+
+    def __init__(self, color):
+        super().__init__()
+        self.color = color
+        print(color)
+        self.setFixedSize(75, 75)
+        self.setStyleSheet(f'background-color : {color}')
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setGeometry(100, 100, 500, 600)
         # Bouton 1
-        btnMessage1 = QPushButton(self)
-        btnMessage1.setFixedSize(150, 40)
-        btnMessage1.setText('Message 1')
-        btnMessage1.move(20, 150)
-        btnMessage1.clicked.connect(lambda: self.evt_btnMessage_clicked(btnMessage1.text()))
+        self.btnMessage1 = QPushButton(self)
+        self.btnMessage1.setFixedSize(150, 40)
+        self.btnMessage1.setText('Message 1')
+        self.btnMessage1.move(20, 150)
+        self.btnMessage1.clicked.connect(lambda: self.evt_btnMessage_clicked(self.btnMessage1.text()))
 
         # Bouton 2
-        btnMessage2 = QPushButton(self)
-        btnMessage2.setFixedSize(150, 40)
-        btnMessage2.setText('Message 2')
-        btnMessage2.move(20, 250)
-        btnMessage2.clicked.connect(lambda: self.evt_btnMessage_clicked(btnMessage2.text()))
+        self.btnMessage2 = QPushButton(self)
+        self.btnMessage2.setFixedSize(150, 40)
+        self.btnMessage2.setText('Message 2')
+        self.btnMessage2.move(20, 250)
+        self.btnMessage2.clicked.connect(lambda: self.evt_btnMessage_clicked(self.btnMessage2.text()))
+
+        self.lblBlue = LblColor('blue')
+        self.lblBlue.setParent(self)
+        self.lblBlue.move(10, 400)
+        self.lblBlue.clicked.connect(lambda: self.label_clicked('blue'))
+
+        self.lblRed = LblColor('red')
+        self.lblRed.setParent(self)
+        self.lblRed.move(100, 400)
+        self.lblRed.clicked.connect(lambda: self.label_clicked('red'))
+
+    def label_clicked(self, color):
+        self.btnMessage1.setStyleSheet(f'background-color: {color}')
+        self.btnMessage2.setStyleSheet(f'background-color: {color}')
+
 
     def evt_btnMessage_clicked(self, msg):
         # Création de la boîte de message
